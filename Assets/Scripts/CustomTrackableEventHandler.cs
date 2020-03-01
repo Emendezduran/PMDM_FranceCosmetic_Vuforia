81382cc933dd0297d044eb04e854b107b6f0b6bd
emendezduran <emendezduran@danielcastelao.org>
2020-03-01 01:26:40 +0100

    DemoAppInit

---
+++ Assets/Scripts/CustomTrackableEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using Vuforia;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+    private bool boolean = false;
+
+    protected override void Start ()
+   {
+       base.Start();
+       var vuforia = VuforiaARController.Instance;
+       vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+       vuforia.RegisterOnPauseCallback(OnPaused);
+   }
+     
+   private void OnVuforiaStarted()
+   {
+       CameraDevice.Instance.SetFocusMode(
+           CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+   }
+     
+   private void OnPaused(bool paused)
+   {
+       if (!paused) // resumed
+       {
+           // Set again autofocus mode when app is resumed
+           CameraDevice.Instance.SetFocusMode(
+               CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+       }
+   }
+
+}
